67fd01e118a418fcd8c32e6f6e6e1c7ec1caccf2
Samuael A <39623015+samuael@users.noreply.github.com>
2024-09-28 21:54:34 +0300

    adding zk processing signature

---
+++ exchanges/apexpro/apexpro_zksigner.go
@@ -0,0 +1,18 @@
+package apexpro
+
+// ZKLinkSigner represents a ZK link signing information
+type ZKLinkSigner struct {
+}
+
+type ZKKeyInfo struct {
+	Seeds         []byte
+	L2Key         string
+	PublicKeyHash []byte
+}
+
+func DriveZKKey(ethAddress string) (*ZKKeyInfo, error) {
+	msgHeader := "ApeX Omni Mainnet"
+	// signature =
+	println(msgHeader)
+	return nil, nil
+}
